7f8e2116bb09429692ffe83281c58f94b0733889
Migliorelli <114029690+miguel-mib@users.noreply.github.com>
2023-02-15 22:20:24 -0300

    prettier changes pls work

--- script.js
+++ script.js
@@ -1,66 +1,66 @@
-const data = new Date()
-const hojeDia = ("0" + data.getDate()).slice(-2)
-const hojeMes = ("0" + (data.getMonth() + 1)).slice(-2)
-const dataHeader = document.querySelector(".hoje")
-dataHeader.innerHTML = `${hojeDia}/${hojeMes}`
+const data = new Date();
+const hojeDia = ("0" + data.getDate()).slice(-2);
+const hojeMes = ("0" + (data.getMonth() + 1)).slice(-2);
+const dataHeader = document.querySelector(".hoje");
+dataHeader.innerHTML = `${hojeDia}/${hojeMes}`;
 
-const botaoMenu = document.querySelector(".botao-menu")
-const materias = document.querySelector(".materias")
-const triangulo = document.querySelector(".botao-triangulo")
+const botaoMenu = document.querySelector(".botao-menu");
+const materias = document.querySelector(".materias");
+const triangulo = document.querySelector(".botao-triangulo");
 botaoMenu.addEventListener("click", () => {
-    botaoMenu.classList.toggle("mostrando")
-    materias.classList.toggle("mostrar");
-    triangulo.classList.toggle("triangulo-rotate");
-})
+  botaoMenu.classList.toggle("mostrando");
+  materias.classList.toggle("mostrar");
+  triangulo.classList.toggle("triangulo-rotate");
+});
 
-const menuOpcoes = document.querySelectorAll(".materias button")
-const secoes = document.querySelectorAll(".seção-materia")
+const menuOpcoes = document.querySelectorAll(".materias button");
+const secoes = document.querySelectorAll(".seção-materia");
 
-secoes.forEach(secao => {   
-    if (!secao.querySelector("div")) {
-        secao.classList.toggle("mostrar")
-        secao.classList.toggle("esconder")
-    }
-})
+secoes.forEach((secao) => {
+  if (!secao.querySelector("div")) {
+    secao.classList.toggle("mostrar");
+    secao.classList.toggle("esconder");
+  }
+});
 
-menuOpcoes.forEach(opcao => {
-    opcao.addEventListener("click", botao => {
-        let botaoClicado = botao.target.value
+menuOpcoes.forEach((opcao) => {
+  opcao.addEventListener("click", (botao) => {
+    let botaoClicado = botao.target.value;
 
-        if (botaoClicado == "mtd") {
-            secoes.forEach(secao => {
-                if (!secao.classList.contains("mostrar")){
-                    secao.classList.toggle("esconder")
-                    secao.classList.toggle("mostrar")
-                }
-
-                if (!secao.querySelector("div")) {
-                    secao.classList.toggle("mostrar")
-                    secao.classList.toggle("esconder")
-                }
-
-                botaoMenu.classList.toggle("mostrando")
-                materias.classList.toggle("mostrar");
-                triangulo.classList.toggle("triangulo-rotate");
-            })
-        } else {
-            secoes.forEach(secao => {
-                let materiaSecao = secao.classList[0]
-    
-                if (!secao.classList.contains("esconder")){
-                    secao.classList.toggle("mostrar")
-                    secao.classList.toggle("esconder")
-                }
-    
-                if (materiaSecao == botaoClicado) {
-                    secao.classList.toggle("esconder")
-                    secao.classList.toggle("mostrar")
-
-                    botaoMenu.classList.toggle("mostrando")
-                    materias.classList.toggle("mostrar");
-                    triangulo.classList.toggle("triangulo-rotate");
-                }
-            });
+    if (botaoClicado == "mtd") {
+      secoes.forEach((secao) => {
+        if (!secao.classList.contains("mostrar")) {
+          secao.classList.toggle("esconder");
+          secao.classList.toggle("mostrar");
         }
-    })
+
+        if (!secao.querySelector("div")) {
+          secao.classList.toggle("mostrar");
+          secao.classList.toggle("esconder");
+        }
+
+        botaoMenu.classList.toggle("mostrando");
+        materias.classList.toggle("mostrar");
+        triangulo.classList.toggle("triangulo-rotate");
+      });
+    } else {
+      secoes.forEach((secao) => {
+        let materiaSecao = secao.classList[0];
+
+        if (!secao.classList.contains("esconder")) {
+          secao.classList.toggle("mostrar");
+          secao.classList.toggle("esconder");
+        }
+
+        if (materiaSecao == botaoClicado) {
+          secao.classList.toggle("esconder");
+          secao.classList.toggle("mostrar");
+
+          botaoMenu.classList.toggle("mostrando");
+          materias.classList.toggle("mostrar");
+          triangulo.classList.toggle("triangulo-rotate");
+        }
+      });
+    }
+  });
 });
